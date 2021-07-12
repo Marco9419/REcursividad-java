b4f9e66f4dae0c299f2f3a07ddff5d54e1e60658
Marco9419 <84870105+Marco9419@users.noreply.github.com>
2021-07-12 17:11:12 -0500

    Add files via upload

---
+++ Ejemplo_Recursividad/src/clases/Clase_Main.java
@@ -0,0 +1,11 @@
+package clases;
+
+public class Clase_Main {
+    
+    public static void main(String args[]) {
+        
+        Recursividad mensajero = new Recursividad();
+        mensajero.Imprimir(1);
+    }
+    
+}
